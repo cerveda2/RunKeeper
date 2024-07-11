5ad2eef2d6fc0503135922e4d604387d964afcbb
David Cervenka <dadacervenka@gmail.com>
2024-07-11 09:24:11 +0200

    Set up session storage

---
+++ core/domain/src/main/java/cz/dcervenka/core/domain/AuthInfo.kt
@@ -0,0 +1,7 @@
+package cz.dcervenka.core.domain
+
+data class AuthInfo(
+    val accessToken: String,
+    val refreshToken: String,
+    val userId: String
+)
---
+++ core/domain/src/main/java/cz/dcervenka/core/domain/SessionStorage.kt
@@ -0,0 +1,6 @@
+package cz.dcervenka.core.domain
+
+interface SessionStorage {
+    suspend fun get(): AuthInfo?
+    suspend fun set(info: AuthInfo?)
+}
