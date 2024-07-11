de61816fc6a4a069ff22ff797ee91af7ce44fa88
David Cervenka <dadacervenka@gmail.com>
2024-07-11 09:35:45 +0200

    Set up token refresh mechanism

---
+++ core/data/src/main/java/cz/dcervenka/core/data/networking/AccessTokenRequest.kt
@@ -0,0 +1,9 @@
+package cz.dcervenka.core.data.networking
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AccessTokenRequest(
+    val refreshToken: String,
+    val userId: String
+)
