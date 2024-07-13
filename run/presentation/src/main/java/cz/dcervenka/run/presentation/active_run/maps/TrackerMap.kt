a27a82ae5e17dff0b745fa841685049ac461d631
David Cervenka <dadacervenka@gmail.com>
2024-07-13 22:00:01 +0200

    Drawing polylines

--- run/presentation/src/main/java/cz/dcervenka/run/presentation/active_run/maps/TrackerMap.kt
+++ run/presentation/src/main/java/cz/dcervenka/run/presentation/active_run/maps/TrackerMap.kt
@@ -85,6 +85,8 @@ fun TrackerMap(
             zoomControlsEnabled = false
         )
     ) {
+        RunKeeperPolylines(locations = locations)
+
         if (!isRunFinished && currentLocation != null) {
             MarkerComposable(
                 currentLocation,
