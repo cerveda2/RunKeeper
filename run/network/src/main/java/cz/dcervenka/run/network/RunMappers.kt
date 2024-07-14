47d92507778c59c82243082c99c7079c5d598832
David Cervenka <dadacervenka@gmail.com>
2024-07-14 19:40:08 +0200

    API implementation

---
+++ run/network/src/main/java/cz/dcervenka/run/network/RunMappers.kt
@@ -0,0 +1,35 @@
+package cz.dcervenka.run.network
+
+import cz.dcervenka.core.domain.location.Location
+import cz.dcervenka.core.domain.run.Run
+import java.time.Instant
+import java.time.ZoneId
+import kotlin.time.Duration.Companion.milliseconds
+
+fun RunDto.toRun(): Run {
+    return Run(
+        id = id,
+        duration = durationMillis.milliseconds,
+        dateTimeUtc = Instant.parse(dateTimeUtc)
+            .atZone(ZoneId.of("UTC")),
+        distanceMeters = distanceMeters,
+        location = Location(lat, long),
+        maxSpeedKmh = maxSpeedKmh,
+        totalElevationMeters = totalElevationMeters,
+        mapPictureUrl = mapPictureUrl
+    )
+}
+
+fun Run.toRunRequest(): RunRequest {
+    return RunRequest(
+        id = id!!,
+        durationMillis = duration.inWholeMilliseconds,
+        distanceMeters = distanceMeters,
+        lat = location.lat,
+        long = location.long,
+        avgSpeedKmh = avgSpeedKmh,
+        maxSpeedKmh = maxSpeedKmh,
+        totalElevationMeters = totalElevationMeters,
+        epochMillis = dateTimeUtc.toEpochSecond() * 1000L
+    )
+}
