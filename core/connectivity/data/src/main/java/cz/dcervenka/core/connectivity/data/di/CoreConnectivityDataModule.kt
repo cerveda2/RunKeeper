5a39518f252bab39a80a8cef0f754f645a7f1d78
David Cervenka <dadacervenka@gmail.com>
2024-07-16 20:41:15 +0200

    Fix koin injection bug

--- core/connectivity/data/src/main/java/cz/dcervenka/core/connectivity/data/di/CoreConnectivityDataModule.kt
+++ core/connectivity/data/src/main/java/cz/dcervenka/core/connectivity/data/di/CoreConnectivityDataModule.kt
@@ -3,11 +3,12 @@ package cz.dcervenka.core.connectivity.data.di
 import cz.dcervenka.core.connectivity.data.WearNodeDiscovery
 import cz.dcervenka.core.connectivity.data.messaging.WearMessagingClient
 import cz.dcervenka.core.connectivity.domain.NodeDiscovery
+import cz.dcervenka.core.connectivity.domain.messaging.MessagingClient
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreConnectivityDataModule = module {
     singleOf(::WearNodeDiscovery).bind<NodeDiscovery>()
-    singleOf(::WearMessagingClient).bind<WearMessagingClient>()
+    singleOf(::WearMessagingClient).bind<MessagingClient>()
 }
